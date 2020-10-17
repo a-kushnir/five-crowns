fdb7e6a1366588bb2c84342045218032592b16bd
Andrew Kushnir <akushnir2009@gmail.com>
2020-10-16 22:00:12 -0600

    Game Process

--- src/app/shared/game/card.ts
+++ src/app/shared/game/card.ts
@@ -14,7 +14,32 @@ export enum CardSuits {
   Diamonds = 'Diamonds',
 }
 
+const base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const suits = Object.keys(CardSuits);
+
 export class Card {
   value: number;
   suit?: string;
+
+  static encode(card: Card): string {
+    let code = 0;
+    if (card.value !== CardValues.Joker) {
+      code = (card.value - 3) * suits.length + suits.indexOf(card.suit) + 1;
+    }
+    return base64[code];
+  }
+
+  static decode(card: string): Card {
+    let code = base64.indexOf(card);
+    if (code < 0) {
+      throw new Error('Invalid Card Code');
+    } else if (code == 0) {
+      return {value: CardValues.Joker};
+    } else {
+      code--;
+      const div = Math.floor(code / suits.length);
+      const mod = code % suits.length;
+      return {value: div + 3, suit: suits[mod]};
+    }
+  }
 }
